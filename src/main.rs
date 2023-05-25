5c646d44d0b5e3361f898bc7bfb3b81b0ab7b522
Roland Fredenhagen <dev@modprog.de>
2023-05-25 16:19:35 +0200

    remove warning

--- src/main.rs
+++ src/main.rs
@@ -149,6 +149,7 @@ fn main() -> Result {
 
 fn recorder(
     updating_config: Updating<Config>,
+    #[cfg_attr(not(eature = "audio_common_msgs_stamped"), allow(unused))]
     frame_id: String,
     audio_channel_send: Sender<(Time, Audio)>,
     audio_channel_recv: Receiver<(Time, Audio)>,
